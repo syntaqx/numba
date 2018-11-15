7ac26656bb14f7c1503f470656b5b91729aaeb93
Chase Hutchins <syntaqx@gmail.com>
2018-11-14 20:18:36 -0700

    simple handling for words and abbr

--- numba.go
+++ numba.go
@@ -5,13 +5,23 @@ import (
 	"strconv"
 )
 
-var abbrSuffix = [4]string{"K", "M", "B", "T"}
+type unit struct {
+	Word string
+	Abbr string
+}
 
-// NumAbbreviate converts integer n to a friendly text abbreviation.
-// For example, 1000 becomes '1K' and 1500 becomes '1.5k'.
-// If n is less than 100, the number is just returned as a string.
-// Accepts octal or hex numbers, returned value is decimal in those cases too.
-func Abbreviate(n int64) string {
+var units = [8]unit{
+	{Word: "thousand", Abbr: "K"},
+	{Word: "million", Abbr: "M"},
+	{Word: "billion", Abbr: "B"},
+	{Word: "trillion", Abbr: "T"},
+	{Word: "quadrillion", Abbr: "P"},
+	{Word: "quintillion", Abbr: "E"},
+	{Word: "sextillion", Abbr: "Z"},
+	{Word: "septillion", Abbr: "Y"},
+}
+
+func unitForInt(n int64) (string, *unit) {
 	num := strconv.FormatInt(n, 10)
 	l := len(num)
 
@@ -21,10 +31,31 @@ func Abbreviate(n int64) string {
 		f = math.Floor(f*10.0+0.5) / 10.0
 
 		idx := l/3 - 1
-		if len(abbrSuffix) >= idx {
-			return strconv.FormatFloat(f, 'f', -1, 64) + abbrSuffix[idx]
+		if len(units) >= idx {
+			return strconv.FormatFloat(f, 'f', -1, 64), &units[idx]
 		}
 	}
 
-	return num
+	// fallback in the event we don't have a valid suffix
+	return "", nil
+}
+
+// Word converts integer n to a friendly text word.
+// For example, 1000 becomes "1 thousand" and 1500 becomes "1.5 thousand".
+// If n is less than 100, the number is just returned as a string.
+// Accepts octal or hex numbers, returned value is decimal in those cases too.
+func Word(n int64) string {
+	if v, u := unitForInt(n); u != nil {
+		return v + " " + u.Word
+	}
+	return strconv.FormatInt(n, 10)
+}
+
+// NumAbbreviate converts integer n to a friendly word abbreviation.
+// @see Word
+func Abbreviate(n int64) string {
+	if v, u := unitForInt(n); u != nil {
+		return v + u.Abbr
+	}
+	return strconv.FormatInt(n, 10)
 }
--- numba_test.go
+++ numba_test.go
@@ -35,3 +35,34 @@ func TestAbbreviate(t *testing.T) {
 		})
 	}
 }
+
+var wordTests = []struct {
+	in  int64
+	out string
+}{
+	{-1, "-1"},
+	{0, "0"},
+	{1, "1"},
+	{100, "100"},
+	{1000, "1 thousand"},
+	{1100, "1.1 thousand"},
+	{1111, "1.1 thousand"},
+	{1500, "1.5 thousand"},
+	{1000000, "1 million"},
+	{1500000, "1.5 million"},
+	{1000000000, "1 billion"},
+	{1500000000, "1.5 billion"},
+	{1000000000000, "1 trillion"},
+	{1500000000000, "1.5 trillion"},
+}
+
+func TestWord(t *testing.T) {
+	for _, tt := range wordTests {
+		t.Run(fmt.Sprintf("%d", tt.in), func(t *testing.T) {
+			s := Word(tt.in)
+			if s != tt.out {
+				t.Errorf("got %q, want %q from %d", s, tt.out, tt.in)
+			}
+		})
+	}
+}
